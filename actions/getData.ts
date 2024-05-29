24d6e4798e23ee2bc6d94850572118bb6c66d040
Alex Straight <alex@Alexs-Laptop.local>
2024-05-28 18:55:49 -0700

    commented out getData file

--- actions/getData.ts
+++ actions/getData.ts
@@ -1,33 +1,33 @@
-"server only";
+// "server only";
 
-import { db } from "@/lib/db";
-import { currentUser } from "@clerk/nextjs/server";
-import "server-only";
+// import { db } from "@/lib/db";
+// import { currentUser } from "@clerk/nextjs/server";
+// import "server-only";
 
-export async function getPlayerRooms() {
-  const user = await currentUser();
+// export async function getPlayerRooms() {
+//   const user = await currentUser();
 
-  if (!user || !user.id) {
-    return {
-      success: false,
-      error: {
-        message: "User does not have a valid session",
-      },
-      data: null,
-    };
-  }
+//   if (!user || !user.id) {
+//     return {
+//       success: false,
+//       error: {
+//         message: "User does not have a valid session",
+//       },
+//       data: null,
+//     };
+//   }
 
-  const playerRooms = await db.room.findMany({
-    where: {
-      playerId: user.id,
-    },
-  });
+//   const playerRooms = await db.room.findMany({
+//     where: {
+//       playerId: user.id,
+//     },
+//   });
 
-  return {
-    success: true,
-    error: null,
-    data: {
-      playerRooms: playerRooms,
-    },
-  };
-}
+//   return {
+//     success: true,
+//     error: null,
+//     data: {
+//       playerRooms: playerRooms,
+//     },
+//   };
+// }
